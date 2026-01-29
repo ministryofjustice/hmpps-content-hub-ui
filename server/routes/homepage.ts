00056d1475fcf8fd681d9ab55fa6acb7b9e2e6fc
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2026-01-29 12:49:01 +0000

    fix e2e tests

--- server/routes/homepage.ts
+++ server/routes/homepage.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response, NextFunction } from 'express'
 import type { Services } from '../services'
 import { Page } from '../services/auditService'
 
-export default function homepageRoutes({ auditService }: Services): Router {
+export default function homepageRoutes({ auditService, exampleService }: Services): Router {
   const router = Router()
 
   router.get('/', async (req: Request, res: Response, next: NextFunction) => {
@@ -12,7 +12,9 @@ export default function homepageRoutes({ auditService }: Services): Router {
         correlationId: req.id,
       })
 
-      throw new Error('Homepage route is functional - pending implementation')
+      const currentTime = await exampleService.getCurrentTime()
+
+      res.render('pages/index', { currentTime })
     } catch (error) {
       next(error)
     }
