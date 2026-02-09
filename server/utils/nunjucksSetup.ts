a55d1a6269e5d02f52a487fbdb348e87a509cd80
Zak Faithfull <zak.faithfull@digital.justice.gov.uk>
2026-02-09 15:05:04 +0000

    remove dps components

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -30,7 +30,6 @@ export default function nunjucksSetup(app: express.Express): void {
       path.join(__dirname, '../../server/views'),
       'node_modules/govuk-frontend/dist/',
       'node_modules/@ministryofjustice/frontend/',
-      'node_modules/@ministryofjustice/hmpps-connect-dps-components/dist/assets/',
     ],
     {
       autoescape: true,
