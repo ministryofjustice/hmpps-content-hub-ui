eaa11a1b1da2630eb138555fb73a196fa598ad53
Ryan Kendall <1352756+ryantk@users.noreply.github.com>
2026-02-25 14:16:51 +0000

    first pass at lazy loading

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
 import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
+import * as lazyLoading from './components/lazyLoading'
 
 govukFrontend.initAll()
 mojFrontend.initAll()
+lazyLoading.initAll()
