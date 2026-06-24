f7476337d29080fcdc74c2e4219f6d26bc6b06e4
Curtis Young <Curtis.Young@justice.gov.uk>
2026-06-24 15:36:51 +0100

    LNP-1610: ammend browse all topics locator role (IT)

--- integration_tests/pages/homePage.ts
+++ integration_tests/pages/homePage.ts
@@ -30,8 +30,8 @@ export default class HomePage extends AbstractPage {
 
   private constructor(page: Page) {
     super(page)
-    this.header = page.getByRole('link', { name: 'Browse all topics' })
-    this.browseAllTopicsButton = page.getByRole('link', { name: 'Browse all topics' })
+    this.header = page.getByRole('button', { name: 'Browse all topics' })
+    this.browseAllTopicsButton = page.getByRole('button', { name: 'Browse all topics' })
     this.updatesHeading = page.getByRole('heading', { name: 'Updates', level: 2 })
     this.featuredHeading = page.getByRole('heading', { name: 'Featured', level: 2 })
     this.recentlyAddedHeading = page.getByRole('heading', { name: 'Recently added', level: 2 })
