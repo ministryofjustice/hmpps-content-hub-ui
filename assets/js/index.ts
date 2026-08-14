71e721100895950818ff502bdf13abc2a76b5da5
Curtis Young <Curtis.Young@justice.gov.uk>
2026-08-14 11:03:38 +0100

    LNP-1768: resolve csp inline style violations

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -2,8 +2,6 @@ import * as govukFrontend from 'govuk-frontend'
 import * as mojFrontend from '@ministryofjustice/frontend'
 import * as lazyLoading from './components/lazyLoading'
 import initFeedbackWidget from './components/feedbackWidget'
-import initAudioPlayer from './components/audioPlayer'
-import initVideoPlayer from './components/videoPlayer'
 import { initActivityTracker } from './components/activityTracker'
 import { initPageNavigation } from './components/pageNavigation'
 
@@ -11,7 +9,5 @@ govukFrontend.initAll()
 mojFrontend.initAll()
 lazyLoading.initAll()
 initFeedbackWidget()
-initAudioPlayer()
-initVideoPlayer()
 initActivityTracker()
 initPageNavigation()
