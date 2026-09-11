876b52bbc91492e72ade7c3b97e8a1522cc60c32
Curtis Young <Curtis.Young@justice.gov.uk>
2026-09-11 14:17:48 +0100

    LNP-1784: ensure page titles match existing content hub

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -12,7 +12,7 @@ export default function nunjucksSetup(app: express.Express): nunjucks.Environmen
   app.set('view engine', 'njk')
 
   app.locals.asset_path = '/assets/'
-  app.locals.applicationName = 'HMPPS Content Hub Ui'
+  app.locals.applicationName = 'Content Hub'
   app.locals.environmentName = config.environmentName
   app.locals.environmentNameColour = config.environmentName === 'PRE-PRODUCTION' ? 'govuk-tag--green' : ''
   let assetManifest: Record<string, string> = {}
